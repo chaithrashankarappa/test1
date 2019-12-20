d9b11626956c87367ee708529116aa87f085fe6b
chaithrashankarappa <58727059+chaithrashankarappa@users.noreply.github.com>
2019-12-20 16:00:34 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,8 @@
 package HelloWorld;
-class Main
+
+public class Main
 {
-     static void main(String[] args)
+    public static void main(String[] args)
     {
         System.out.println("Hello world");
     }
